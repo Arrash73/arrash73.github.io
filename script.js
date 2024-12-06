edbb1f001d38dbe0b044ea7e8be89ac1465d7290
Arrash73 <arifrahmansholeh7@gmail.com>
2024-12-06 12:15:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Portofolio Arif Rahman Sholeh berhasil dimuat.");
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-  const cvButton = document.querySelector('.cv-button');
-  
-  cvButton.addEventListener('click', function () {
-    alert('CV sedang diunduh. Terima kasih telah mengunjungi portofolio saya!');
-  });
-});
